e146ce29e674914b5bdeaf4f525bcd9f8d473a05
Dajiege <5175248@qq.com>
2017-04-26 13:16:27 +0800

    解决数据获取失败的情况

--- task/sendmail.js
+++ task/sendmail.js
@@ -8,47 +8,52 @@ var send = function(cb){
     .charset('gbk')
     .end(function (err,r) {
         var arr = [];
-        var data_  = eval(r.text);
-        var data = data_[0].stockList;
-        var transporter = nodemailer.createTransport('SMTP',{
-            host: "smtp.163.com",
-            secureConnection: true,
-            port:465,
-            auth: {
-                user: '***@163.com',
-                pass: '***'
+        if(r){
+            var data_  = eval(r.text);
+            var data = data_[0].stockList;
+            var transporter = nodemailer.createTransport('SMTP',{
+                host: "smtp.163.com",
+                secureConnection: true,
+                port:465,
+                auth: {
+                    user: '***@163.com',
+                    pass: '***'
+                }
+            });
+            function jsonpCallback(a){
+                return a;
             }
-        });
-        function jsonpCallback(a){
-            return a;
-        }
-        for(var i = 0; i < data.length; i++){
-            if(data[i].hqzqdm === "403011"){
-                arr.push(data[i].hqjrzd);
-                arr.push(data[i].hqzjcj);
-            }
-        }
-        var mailOptions = {
-            from: '"lin" <18621303280@163.com>', // sender address
-            to: '5175248@qq.com', // list of receivers
-            subject: '涨跌情况', // Subject line
-            text: '抽奖', // plain text body
-            html: '<b>涨跌幅：</b>' + arr[0] + '<br><b>当前价：</b>' + arr[1]// html body
-        };
-        mongo.selectData({'id': 'price'},'mystatus',{},function(result){
-            if(result.length !== 0){
-                var price = result[0].price;
-                var push = result[0].pushprice;
-                var flag = result[0].status;
-                if(parseFloat(arr[1]) < (price+push) && !flag){
-                    transporter.sendMail(mailOptions,function(err,res){
-                        console.log(res);
-                        var data = {'status' : true};
-                        mongo.updateData(data,{'id': 'price'},'mystatus');
-                    })
+            for(var i = 0; i < data.length; i++){
+                if(data[i].hqzqdm === "403011"){
+                    arr.push(data[i].hqjrzd);
+                    arr.push(data[i].hqzjcj);
                 }
             }
-        });
+            var mailOptions = {
+                from: '"lin" <18621303280@163.com>', // sender address
+                to: '5175248@qq.com,522428268@qq.com', // list of receivers
+                subject: '涨跌情况', // Subject line
+                text: '抽奖', // plain text body
+                html: '<b>涨跌幅：</b>' + arr[0] + '<br><b>当前价：</b>' + arr[1]// html body
+            };
+            mongo.selectData({'id': 'price'},'mystatus',{},function(result){
+                if(result.length !== 0){
+                    var price = result[0].price;
+                    var push = result[0].pushprice;
+                    var flag = result[0].status;
+                    if(parseFloat(arr[1]) < (price+push) && !flag){
+                        transporter.sendMail(mailOptions,function(err,res){
+                            console.log(res);
+                            var data = {'status' : true};
+                            mongo.updateData(data,{'id': 'price'},'mystatus');
+                        })
+                    }
+                }
+            });
+        } else {
+            console.log("获取数据失败");
+        }
+        
         if(cb){
             cb(arr);
         }
